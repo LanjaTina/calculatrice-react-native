dc09d7cc3e4098685ec2c29c65566d4c70b6b7d7
LanjaTina Randriananahary <111855158+LanjaTina@users.noreply.github.com>
2024-03-09 19:30:35 +0100

    Update CalculatorButton.js

--- src/components/CalculatorButton.js
+++ src/components/CalculatorButton.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, TouchableOpacity, Text } from 'react-native';
 
+
 export default function CalculatorButton({ onPress, value }) {
   return (
     <TouchableOpacity style={styles.button} onPress={() => onPress(value)}>
@@ -8,6 +9,8 @@ export default function CalculatorButton({ onPress, value }) {
     </TouchableOpacity>
   );
 }
+
+
 
 const styles = StyleSheet.create({
   button: {
